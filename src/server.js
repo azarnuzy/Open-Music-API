ee9e9cb46b98d2bfeeeafeea66ed2b7b62c4bfb6
azarnuzy <azarnuzy@gmail.com>
2023-06-14 09:30:35 +0700

    add song service

--- src/server.js
+++ src/server.js
@@ -2,11 +2,16 @@ require('dotenv').config()
 const Hapi = require('@hapi/hapi')
 
 const albums = require('./api/albums')
+const songs = require('./api/songs')
+
 const AlbumsService = require('./services/postgres/AlbumsService')
 const AlbumsValidator = require('./validator/albums')
+const SongsService = require('./services/postgres/SongsService')
+const SongsValidator = require('./validator/songs')
 
 const init = async () => {
   const albumsService = new AlbumsService()
+  const songsService = new SongsService()
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -24,6 +29,13 @@ const init = async () => {
       options: {
         service: albumsService,
         validator: AlbumsValidator,
+      },
+    },
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
       },
     },
   ])
